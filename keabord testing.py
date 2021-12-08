cf4e4af75c0b4f5680a9ab481aa1708f7cdca808
simpleOFkinda <82427844+galaxyKnH@users.noreply.github.com>
2021-12-08 09:08:02 +0700

    Add files via upload

---
+++ keabord testing.py
@@ -0,0 +1,27 @@
+from pynput.keyboard import Key, Controller
+
+
+keyboard = Controller()
+
+
+a = 2
+
+if a == 2:    
+    keyboard.press("h")
+    keyboard.press("e")
+    keyboard.press("l")
+    keyboard.press("l")
+    keyboard.press("o")
+
+
+    
+#hey dude go to cmd and use the command "pip install pynput"
+#with out the two top dots for more help read help read me
+
+
+if a == 2:
+keyboard.press("h") time.sleep(0.5) keyboard.press("e") time.sleep(0.5) keyboard.press("l") time.sleep(0.5) keyboard.press("l") time.sleep(0.5) keyboard.press("o")
+    
+
+
+
